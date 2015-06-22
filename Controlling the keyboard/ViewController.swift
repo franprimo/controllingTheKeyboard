e6bc8092e119da47040cab11da98a808a903376d
Fran Primo Ruiz <FranPrimo@MacBook-de-Fran.local>
2015-06-22 18:55:55 +0200

    Initial Commit

---
+++ Controlling the keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Controlling the keyboard
+//
+//  Created by Fran Primo Ruiz on 22/6/15.
+//  Copyright (c) 2015 Fran Primo Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
